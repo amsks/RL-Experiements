0b48ac479a9e3a87e7aa24ac10bfc2add8cf28a9
amsks <adityak735@gmail.com>
2020-09-21 01:07:44 +0200

    3-1. Added reward tracking

--- qlearning-1.py
+++ qlearning-1.py
@@ -14,34 +14,46 @@
 
 import gym
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 env = gym.make("MountainCar-v0")
 
 LEARNING_RATE = 0.1
 
 DISCOUNT = 0.95
-EPISODES = 25000
-SHOW_EVERY = 3000
+EPISODES = 10000
+SHOW_EVERY = EPISODES
+STAT_EVERY = 50
 
+# Q-table Settings
 DISCRETE_OS_SIZE = [20, 20]
 discrete_os_win_size = (env.observation_space.high - env.observation_space.low)/DISCRETE_OS_SIZE
+q_table = np.random.uniform(low=-2, high=0, size=(DISCRETE_OS_SIZE + [env.action_space.n]))
 
 # Exploration settings
 epsilon = 1  # not a constant, qoing to be decayed
 START_EPSILON_DECAYING = 1
-END_EPSILON_DECAYING = EPISODES//2
+END_EPSILON_DECAYING = EPISODES
 epsilon_decay_value = epsilon/(END_EPSILON_DECAYING - START_EPSILON_DECAYING)
 
+# Reward tracking mechanism
+ep_rewards = []
+aggr_ep_rewards = {
+    'ep'  : [],     # Disctionary to track episodes
+    'avg' : [],     # Trailing average of average reward
+    'min' : [],     # worst model we had 
+    'max' : []      # Best model 
+}
 
-q_table = np.random.uniform(low=-2, high=0, size=(DISCRETE_OS_SIZE + [env.action_space.n]))
-
-
+#Function to get the discrete value of a Q state
 def get_discrete_state(state):
     discrete_state = (state - env.observation_space.low)/discrete_os_win_size
     return tuple(discrete_state.astype(np.int))  # we use this tuple to look up the 3 Q values for the available actions in the q-table
 
 
 for episode in range(EPISODES):
+    episode_reward = 0
     discrete_state = get_discrete_state(env.reset())
     done = False
 
@@ -62,7 +74,7 @@ for episode in range(EPISODES):
 
 
         new_state, reward, done, _ = env.step(action)
-
+        episode_reward += reward
         new_discrete_state = get_discrete_state(new_state)
 
         if episode % SHOW_EVERY == 0:
@@ -89,12 +101,30 @@ for episode in range(EPISODES):
         elif new_state[0] >= env.goal_position:
             #q_table[discrete_state + (action,)] = reward
             q_table[discrete_state + (action,)] = 0
-            print("Done on episode :" + str(episode))
+            print(f"Done on episode: {episode}" )
         discrete_state = new_discrete_state
 
     # Decaying is being done every episode if episode number is within decaying range
     if END_EPSILON_DECAYING >= episode >= START_EPSILON_DECAYING:
         epsilon -= epsilon_decay_value
 
+    ep_rewards.append(episode_reward)
+
+    if not episode % STAT_EVERY:
+        average_reward = sum(ep_rewards[-STAT_EVERY:])/STAT_EVERY
+        aggr_ep_rewards['ep'].append(episode)
+        aggr_ep_rewards['avg'].append(average_reward)
+        aggr_ep_rewards['max'].append(max(ep_rewards[-STAT_EVERY:]))
+        aggr_ep_rewards['min'].append(min(ep_rewards[-STAT_EVERY:]))
+
+        print(f'Episode: {episode}, average reward: {average_reward}, current epsilon: {epsilon}')
+
 
 env.close()
+
+plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['avg'], label="average rewards")
+plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['max'], label="max rewards")
+plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['min'], label="min rewards")
+plt.legend(loc=4)
+plt.grid(True)
+plt.show()
